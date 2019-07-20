02507ec4c60780a9b3fd6c982870cd7058ce3cbf
devoptimist <sbrown@chef.io>
2019-07-20 23:45:18 +0200

    test data added

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+default['test_1']['name'] = 'test_1'
+default['test_1']['channel'] = ''
+default['test_1']['install_version'] = ''
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,12 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+
+template '/var/tmp/test_1_details.sh' do
+  source 'details.sh.erb'
+  variables(
+    name: node['test_1']['name'],
+    channel: node['test_1']['channel'],
+    install_version: node['test_1']['install_version']
+  )
+end
